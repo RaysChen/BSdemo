5ae7f8e7d3790a1436202909b2a3cee1bd2f2ed4
RaysChen <543114160@qq.com>
2016-04-14 15:17:18 +0800

    百思demo

---
+++ 百思demo/百思demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思demo
+//
+//  Created by 陈曦 on 16/4/14.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思demo/百思demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思demo
+//
+//  Created by 陈曦 on 16/4/14.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
