d4bc566bed9c4741bfa29c01c522ee86ca3331c7
RaysChen <543114160@qq.com>
2016-06-03 19:03:12 +0800

    BSdemo
    精华模块，顶部

--- 百思demo/百思demo/classes/Essence_精华/controller/CXEssenceViewController.m
+++ 百思demo/百思demo/classes/Essence_精华/controller/CXEssenceViewController.m
@@ -11,7 +11,14 @@
 
 
 @interface CXEssenceViewController ()
-
+/*标签栏底部的红色指示器*/
+@property (nonatomic, weak)UIView *indicatorView;
+/*当前选中的按钮*/
+@property (nonatomic , weak)UIButton *selectedButton;
+/*顶部的所有标签*/
+@property (nonatomic, weak)UIView *titlesView;
+/*底部的所有内容*/
+@property (nonatomic , weak)UIScrollView *contentView;
 @end
 
 @implementation CXEssenceViewController
@@ -20,6 +27,16 @@
     [super viewDidLoad];
     
     
+    [self setupNav];
+    [self setupTitlesView];
+}
+
+
+/**
+ *  设置底部导航栏
+ */
+-(void)setupNav
+{
     //设置导航栏标题
     self.navigationItem.titleView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"MainTitle"]];
     
@@ -29,10 +46,37 @@
     
     // 设置背景色
     self.view.backgroundColor = CXGlobalBg;
-    
+
 }
 
+/**
+ *  设置顶部的标签栏
+ */
+-(void)setupTitlesView{
 
+ //标签栏整体
+    UIView *titlesView = [[UIView alloc]init];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
+    titlesView.width = self.view.width;
+    titlesView.height = 35;
+    titlesView.y= 64;
+    [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+//顶部标签下方的红色指示
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.tag = -1;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    self.indicatorView = indicatorView;
+
+
+
+
+
+
+}
 
 
 - (void)tagClick
