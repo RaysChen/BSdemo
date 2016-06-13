38d3b68fca73c146788703e93cacde39d1604ad7
RaysChen <543114160@qq.com>
2016-06-13 09:49:21 +0800

    BSdemo

---
+++ 百思demo/百思demo/classes/Essence_精华/view/CXTopicCell.h
@@ -0,0 +1,13 @@
+//
+//  CXTopicCell.h
+//  百思demo
+//
+//  Created by 陈曦 on 16/6/8.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXTopicCell : UITableViewCell
+
+@end
---
+++ 百思demo/百思demo/classes/Essence_精华/view/CXTopicCell.m
@@ -0,0 +1,23 @@
+//
+//  CXTopicCell.m
+//  百思demo
+//
+//  Created by 陈曦 on 16/6/8.
+//  Copyright © 2016年 chenxi. All rights reserved.
+//
+
+#import "CXTopicCell.h"
+
+@implementation CXTopicCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
